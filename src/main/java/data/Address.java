b6cea58befdad86909d05f768f4cd488ad56ba3e
olegraspopov <om-raspopov@ya.ru>
2017-11-02 01:13:13 +0300

    Data files, ignite config

---
+++ src/main/java/data/Address.java
@@ -0,0 +1,12 @@
+package data;
+
+public class Address {
+    private String street;
+    private int zip;
+
+    public Address(String street, int zip) {
+        this.street = street;
+        this.zip = zip;
+    }
+
+}
---
+++ src/main/java/ignite/RunServerNode.java
@@ -0,0 +1,14 @@
+package ignite;
+
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+
+/**
+ * Created by sbt-raspopov-om on 27/10/2017.
+ */
+public class RunServerNode {
+    public static void main(String[] args) {
+        Ignite ignite = Ignition.start(GridConfig.igniteConfiguration(false));
+    }
+}
